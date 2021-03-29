f9369a6035da08c11eaa9166b08c523cff0438f6
Tatyana Komolyh <tatyana.comolyh@yandex.ru>
2021-03-29 12:35:11 +0300

    rh4h4h

--- ЛР1/Source1.cpp
+++ ЛР1/Source1.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<cmath>
+#include<algorithm>
 #include<ctime>
 using namespace std;
 
@@ -29,7 +30,7 @@ double SKO(vector<double> x, double MO) {
 }
 
 int main() {
-	int n = 8, m = 5, lambda = 82, mu = 6, j_k = 400, k_j = 7;
+	int _n = 8, m = 5, lambda = 82, mu = 6, j_k = 400, k_j = 7;
 	srand(time(0));
 
 	FILE* file, *file1;
@@ -39,12 +40,13 @@ int main() {
 		double dt = 0, r = 0, q = 0, t_pr = 0, t_obs = 0, count_N_busy = 0, count_M_busy = 0, t_no = 0, index = 1000, t_m = 0;
 		vector<pair<bool, double>> n_busy, m_busy;
 		vector<int> m_busy_n; // хранит номер канала для очереди
-		n = 8;
+		int n = _n;
 		n_busy.assign(n, make_pair(false, 0));
 		m_busy.assign(m, make_pair(false, 0));
 		m_busy_n.assign(m, 1000);
-		if (j == 51) {
-			fprintf_s(file, "%4s | %5s | %5s | %5s | %6s | %3s | %3s | %3s | %6s | %7s | %8s | \n", "i", "r", "q", "dt", "t_pr", "n", "m", "N", "t_no", "t_obs", "t_ko");
+		if (j == 0) {
+			fprintf_s(file, "%4s | %5s | %5s | %5s | %6s | %3s | %3s | %3s | %6s | %7s | %8s | \n", 
+				"i", "r", "q", "dt", "t_pr", "n", "m", "N", "t_no", "t_obs", "t_ko");
 		}
 		for (int i = 0; i < 1000; ++i) {
 			double min = 100000000;
@@ -53,12 +55,28 @@ int main() {
 			int min_m_index = 1000;
 			bool exp = false;
 			if (i == j_k) {
-				if (count_N_busy == n)count_N_busy--;
-				for (int k = 0; k < n - k_j; ++k) {
-					n_busy[n_busy.size() - 1 - k].first = false;
-					n_busy[n_busy.size() - 1 - k].second = 10000;
+				if (count_N_busy == n) count_N_busy--;
+				vector<int> n_del;
+				vector<pair<double, int>> min_del;
+				for (int c = 0; c < n; ++c) {
+					min_del.push_back(make_pair(n_busy[c].second, c));
 				}
-				n = k_j;
+				sort(min_del.begin(), min_del.end());
+				for (int k = 0; k < n - k_j; ++k) {
+					n_busy[min_del[k].second].first = false;
+					n_busy[min_del[k].second].second = 10000;
+					n_del.push_back(min_del[k].second);
+				}
+				for (int k = 0; k < n_del.size(); ++k) {
+					for (int l = 0; l < m_busy_n.size(); ++l) {
+						if (m_busy_n[l] == n_del[k]) {
+							m_busy[l].second = 0;
+							m_busy[l].first = false;
+							m_busy_n[l] = 1000;
+							count_obs[j]--;
+						}
+					}
+				}
 			}
 			if (i != 0) {
 				r = abs(2.0 * rand() / RAND_MAX - 1.0);
@@ -79,20 +97,20 @@ int main() {
 						m_busy[k].first = false;
 						count_M_busy--;
 					}
-					if (min > m_busy[k].second && m_busy[k].first) {
-						min_m = n_busy[k].second;
+					if (min_m > m_busy[k].second && m_busy[k].first) {
+						min_m = m_busy[k].second;
 						min_m_index = k;
 					}
 				}
 			}
 			q = abs(2.0 * rand() / RAND_MAX - 1.0);
 			t_obs = Function_t_obs(q, mu);
-			if (j == 51) {
+			if (j == 0) {
 				fprintf_s(file, "%4i | %3.3f | %3.3f | %3.3f | %6.3f | %3.0f | %3.0f |", i+1, r, q, dt, t_pr, count_N_busy, count_M_busy);
 			}
 			if (count_N_busy < n) {
 				for (int k = 0; k < n; ++k) {
-					if (!n_busy[k].first) {
+					if (!n_busy[k].first && n_busy[k].second < 10000) {
 						t_no = t_pr;
 						n_busy[k].second = t_no + t_obs;
 						n_busy[k].first = true;
@@ -133,7 +151,7 @@ int main() {
 							m_busy[min_m_index].second = t_pr;
 							t_no = n_busy[m_busy_n[min_m_index]].second;
 							n_busy[min_index].second = t_no + t_obs;
-							index = min_index;
+							index = m_busy_n[min_m_index];
 							if (i < 999){
 								t_m += m_busy[min_m_index].second - t_pr;
 								Mt_m[j]++;
@@ -148,11 +166,11 @@ int main() {
 			if (!exp) {
 				count_obs[j]++;
 				Mn_busy[j] += t_obs;
-				if (j == 51) {
+				if (j == 0) {
 					fprintf_s(file, " %3.0f | %6.3f | %6.5f | %8.5f | \n", index + 1, t_no, t_obs, n_busy[index].second);
 				}
 			}
-			else if (j == 51) {
+			else if (j == 0) {
 				fprintf_s(file, " %3s | %6s | %7s | %8s | \n", "__", "__", "__", "__");
 			}
 			if (i == 999) {
@@ -166,7 +184,6 @@ int main() {
 			}
 		}
 	}
-	fclose(file);
 
 	fopen_s(&file1, "output.txt", "w");
 	fprintf_s(file1, "Число обслуженных заявок: \n Математическое ожидание = %3.3f \n СКО = %3.3f \n\n", MO(count_obs), SKO(count_obs, MO(count_obs)));
@@ -178,4 +195,5 @@ int main() {
 	fprintf_s(file1, "Среднее число занятых мест в очереди: \n Математическое ожидание = %3.3f \n СКО = %3.3f \n\n", MO(Mm_busy), SKO(Mm_busy, MO(Mm_busy)));
 	
 	fclose(file1);
+	fclose(file);
 }
